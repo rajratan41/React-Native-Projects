6e94dd33b0c8be2a23a606ba3db4efe45c987f3b
Raj Ratan <raj808341@gmail.com>
2023-06-03 16:53:27 +0530

    created Elevated Card Component

---
+++ Styler02/components/ElevatedCards.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const ElevatedCards = () => {
+  return (
+    <View>
+      <Text>Elevated Cards</Text>
+    </View>
+  );
+};
+
+export default ElevatedCards;
+
+const styles = StyleSheet.create({});
